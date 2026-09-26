b3eb3a8e63c59902a0657456b3e272e98e018a38
BlueDragon33 <dinhnam331007@gmail.com>
2026-09-26 08:21:21 +0700

    feat: export Prompt Projection history adapter

--- packages/persistence/src/index.ts
+++ packages/persistence/src/index.ts
@@ -4,3 +4,4 @@ export * from "./project-profile-repository";
 export * from "./work-quality-repository";
 export * from "./governance-repository";
 export * from "./release-repository";
+export * from "./prompt-history-repository";
